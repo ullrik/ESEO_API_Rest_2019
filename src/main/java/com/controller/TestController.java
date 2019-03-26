d09bdd1b13630ecaa1f010f4b982d69262ef9a3d
morisseau mickael <mickael.morisseau@capgemini.com>
2019-03-26 13:44:17 +0100

    commit

---
+++ src/main/java/com/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping("/path")
+public class TestController {
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	@ResponseBody
+	public String get(@RequestParam(required = false, value="value") String value) {
+		System.out.println("Appel GET");
+		System.out.println("value : "+value);
+
+		return value;
+	}
+}
